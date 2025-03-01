9aaaf1225950bb16b8a5fa881e30f449d431f826
Wency <wency@gmail.com>
2025-03-01 13:45:07 +0800

    Update App.tsx to remove random quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import CreateAccount from './SignupPage'; // Ensure you have this component
 import MoviePage from './moviePage'; // Import the MoviePage component
-import axios from 'axios';
 
 const App: React.FC = () => {
-  const [quote, setQuote] = useState<string>('');
-
-  useEffect(() => {
-    console.log('Making request to http://localhost:5174/');
-    axios.get('http://localhost:5174/')
-      .then(res => {
-        console.log('Response received:', res);
-        setQuote(res.data.content);
-      })
-      .catch(err => {
-        console.log('Error occurred:', err);
-      });
-  }, []);
-
   return (
     <Router>
       <Routes>
@@ -27,9 +12,6 @@ const App: React.FC = () => {
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/movies" element={<MoviePage />} /> {/* Add the route for MoviePage */}
       </Routes>
-      <div>
-        <p>Random Quote: {quote}</p>
-      </div>
     </Router>
   );
 };
