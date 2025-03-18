8c83abf748f266b1dc347cadba77ec3e024c892e
Wency <wencywego21@gmail.com>
2025-03-18 15:36:42 +0800

    3:36 update

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
 
   return (
     <Router>
-        <ToastContainer position="top-center" autoClose={3000} />
+        <ToastContainer position="top-center" autoClose={750} />
       <Routes>
       {/* Public Routes (redirect if logged in) */}
       <Route
